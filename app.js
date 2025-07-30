83ecb6b122458cabfe58b227a6fd5ab2ea0deafd
andre <lauralme96@gmail.com>
2025-07-29 19:08:47 -0500

    css

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const taskInput = document.getElementById('task-input');
+    const addBtn = document.getElementById('add-btn');
+    const tasksContainer = document.getElementById('tasks-container');
+    const alertContainer = document.getElementById('alert-container');
+
+    // Función para mostrar alerta
+     
+                   
